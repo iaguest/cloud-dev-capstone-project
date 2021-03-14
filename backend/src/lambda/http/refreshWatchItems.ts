e477877d6be75477cb23bb56ea8c8abd5cb654b5
ianguest <iag.1.apps@gmail.com>
2021-03-14 14:44:33 +0000

    refactor

--- backend/src/lambda/http/refreshWatchItems.ts
+++ backend/src/lambda/http/refreshWatchItems.ts
@@ -2,33 +2,20 @@ import 'source-map-support/register'
 
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
 
-import { getUserId } from '../utils'
+import { getUserId, buildHttpResponse } from '../utils'
 import { refreshWatchItems } from '../../businessLogic/watchItems'
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  console.log('Processing event: ', event)
 
   const userId: string = getUserId(event)
-  const headers = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Credentials': true
-  }
 
   try {
     await refreshWatchItems(userId)
   }
   catch (e) {
-    return {
-      statusCode: 400,
-      headers: headers,
-      body: JSON.stringify({
-        error: `Refresh failed: ${e.message}`
-      })
-    }
+    return buildHttpResponse(500, { error: `Refresh failed: ${e.message}` })
   }
 
-  return {
-    statusCode: 200,
-    headers: headers,
-    body: ''
-  }
+  return buildHttpResponse(200, {})
 }
