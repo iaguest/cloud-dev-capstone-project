e45148ef4f6e1a9170ba9e8685f3a6c7afde3b75
ianguest <iag.1.apps@gmail.com>
2021-03-17 14:54:28 +0000

    add logic for deciding whether to trigger a notification or not when an alert is triggered

--- backend/src/lambda/dynamoDb/checkAlertStatus.ts
+++ backend/src/lambda/dynamoDb/checkAlertStatus.ts
@@ -1,10 +1,38 @@
 import { DynamoDBStreamEvent, DynamoDBStreamHandler } from 'aws-lambda'
 import 'source-map-support/register'
 
+import { DbAccess } from '../../dataLayer/dbAccess'
+
 export const handler: DynamoDBStreamHandler = async (event: DynamoDBStreamEvent) => {
   console.log('Processing events batch from DynamoDB', JSON.stringify(event))
 
+  const dbAccess = new DbAccess()
   for (const record of event.Records) {
-    console.log('Processing record', JSON.stringify(record))
+    if (record.eventName !== 'MODIFY') {
+      console.log(`Skipping record for eventID:${record.eventID} which is unrelated to modification`)
+      continue
+    }
+
+    console.log('Processing modified record', JSON.stringify(record))
+
+    const previousItem = record.dynamodb.OldImage
+    if (previousItem === undefined) {
+      console.log('Skip undefined OldImage...')
+      continue
+    }
+
+    const isPreviousAlert = previousItem.alertTriggered.BOOL
+    if (isPreviousAlert) {
+      console.log('Alert trigger already set, ignore as notification will already have been handled')
+      continue
+    }
+
+    const currentWatchItem = await dbAccess.getWatchItem(previousItem.userId.S, previousItem.watchId.S)
+    if (!currentWatchItem.alertTriggered) {
+      console.log('Alert trigger not currently set, ignore this change')
+      continue
+    }
+
+    console.log("**** TRIGGER NOTIFICATION ****")
   }
 }
